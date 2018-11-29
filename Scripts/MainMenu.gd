c88371ad0ec2c93f34ccb75cf82e611de8a2d3db
Syed zainul abedin <szsimplesyed93@gmail.com>
2018-11-29 23:20:48 +0530

    Added credits btn

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -7,6 +7,8 @@ func _ready():
 func _on_PlayGame_button_up():
 	get_tree().change_scene("res://Scenes/SelectionScreen.tscn")
 
+func _on_Credits_button_up():
+	get_tree().change_scene("res://Scenes/credits.tscn")
 
 func _on_Exit_button_up():
 	get_tree().quit()
